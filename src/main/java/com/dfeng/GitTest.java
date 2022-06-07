779367b648db908d33ed97ffef536e7682c181d3
dengFengWJJ <dubobisi9438@163.com>
2022-06-07 11:45:15 +0800

    master 2 commit

--- src/main/java/com/dfeng/GitTest.java
+++ src/main/java/com/dfeng/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("Hello,Git hot fix!");
         System.out.println("Hello,Git master!");
         System.out.println("Hello,Git hot fix2!");
+        System.out.println("Hello,Git hot fix2!");
     }
 }
